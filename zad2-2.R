1799b3b3a4f5eb3ce581b353b8b8312fb958dd80
Julia Stępień <38504024+stepien-j@users.noreply.github.com>
2018-05-19 17:53:44 +0200

    Delete zad2-2.R

--- zad2-2.R
+++
@@ -1,9 +0,0 @@
-today <- Sys.Date()
-deadline <- as.Date("2022-06-30")
-
-hours <- as.numeric(difftime(deadline, today), units="hours")
-additional <- as.numeric(difftime(strptime("23:59", format = "%H:%M"), 
-                                  strptime("13:17", format = "%H:%M")), units="hours") # do konca dzisiaj
-hours <- hours + additional
-
-paste("do końca studiów zostało", hours, "godzin")
